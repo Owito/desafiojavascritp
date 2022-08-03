223ef4921e09d45121db29ca051132c45e72d37f
Carlos G <fgoguerra@gmail.com>
2022-08-02 22:37:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let arcoiris = prompt("Ingresa el color que deseas escoger \n1. amarillo \n2 azul \n3 rojo ")
+
+switch (arcoiris) {
+    case "amarillo":
+        alert("Ud. Ud. escogío el color de")
+        break;
+    case "azul":
+        alert("Ud. escogío el color de")
+        break;
+    case "negro":
+        alert("Ud. escogío el color de")
+        break;
+    default:
+        alert("este color pertenece a esta temática")
+        break;
+}
